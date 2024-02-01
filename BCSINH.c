1b9c53839f419711996cd5ab7cec4abaae488c83
luonginfosec <156890675+luonginfosec@users.noreply.github.com>
2024-02-01 13:20:02 +0700

    Update BCSINH.c

--- BCSINH.c
+++ BCSINH.c
@@ -1,1 +1,30 @@
 
+#include<stdio.h>
+#include<math.h>
+int a[550] = {};
+int n,isFinal;
+void solve(){
+	int i = n;
+	while(i >= 1 && a[i] == 1){
+		a[i] = 0;
+		i--;
+	}
+	if(i==0){
+		isFinal = 1;
+	
+	}
+	else{
+		a[i] = 1;
+	}
+}
+int main(){
+	isFinal = 0;
+	scanf("%d",&n);
+	while(!isFinal){
+		for(int i = 1;  i<=n ; i++){
+			printf("%d",a[i]);
+		}
+		printf("\n");
+		solve();
+	}
+}
